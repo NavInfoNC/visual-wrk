532ae4bd4c17cf53b478718e73e35b70288aa5d2
liudf <liudf@mapbar.com>
2019-04-30 11:44:33 +0800

    update License info

--- src/wrk.c
+++ src/wrk.c
@@ -1,4 +1,19 @@
 // Copyright (C) 2012 - Will Glozer.  All rights reserved.
+/*
+   Copyright (C)2018 NavInfo Co.,Ltd. All right reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 #include "wrk.h"
 #include "script.h"
@@ -85,7 +100,13 @@ static void usage() {
            "    -v, --version          Print version details      \n"
            "                                                      \n"
            "  Numeric arguments may include a SI unit (1k, 1M, 1G)\n"
-           "  Time arguments may include a time unit (2s, 2m, 2h)\n");
+           "  Time arguments may include a time unit (2s, 2m, 2h) \n"
+           "  --------------------------------------------------- \n"
+           "  Copyright (C)2018 NavInfo Co.,Ltd. All right reserved.\n"
+           "  main page: https://github.com/NavInfoNC/visual-wrk \n"
+           "  LICENSE: https://github.com/NavInfoNC/visual-wrk/blob/master/LICENSE \n"
+           "  NOTICE: https://github.com/NavInfoNC/visual-wrk/blob/master/NOTICE \n"
+           );
 }
 
 static char *get_template(const char *template_name) {
